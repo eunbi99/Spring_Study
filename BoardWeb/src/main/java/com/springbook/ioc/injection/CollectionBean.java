6fbe65d18ac4b28ab6b6eac3bf83c0c814004680
eunbi99 <silverbi99@naver.com>
2021-06-01 22:06:29 +0900

    21/06/01

---
+++ BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBean.java
@@ -0,0 +1,19 @@
+package com.springbook.ioc.injection;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class CollectionBean {
+   
+   private Properties addressList;
+
+public Properties getAddressList() {
+	return addressList;
+}
+
+public void setAddressList(Properties addressList) {
+	this.addressList = addressList;
+}
+
+
+}

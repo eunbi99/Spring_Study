dc6fd3570663d9a5671e1081bfd9f63cfa312fca
eunbi99 <silverbi99@naver.com>
2021-06-03 16:38:52 +0900

    AOP(2)

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -13,7 +13,7 @@ import com.springbook.biz.board.common.JDBCUtil;
 
 //DAO(Data Access Object)
 
-@Repository("boardDAO")
+//@Repository("boardDAO")
 public class BoardDAO {
 	//JDBC 관련 변수
 	private Connection conn=null;
--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -12,13 +12,14 @@ import com.springbook.biz.common.Log4jAdvice;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService{
 	@Autowired
-	private BoardDAO boardDAO;
+	private BoardDAOSpring boardDAO;
 
 	
 	public void insertBoard(BoardVO vo) {
 //		if(vo.getSeq() ==0 ) {
 //			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
 //		}
+		boardDAO.insertBoard(vo);
 		boardDAO.insertBoard(vo);
 		
 	}
